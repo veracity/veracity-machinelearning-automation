7e6b0ced6c9ac2fb6ed565d6e77a2e3ec1e67086
Pawel Lehmann <Pawel.Lehmann@dnvgl.com>
2017-09-04 10:14:36 +0200

    Add Machine Learning Consume part + sample test.

---
+++ MachineLearningConsumer/WebServiceConsumer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineLearningConsumer
+{
+    /// <summary>
+    /// Based on Request Response Execution API Documentation for Retrain Model provided by Microsoft.
+    /// https://docs.microsoft.com/en-us/azure/machine-learning/machine-learning-consume-web-services
+    /// 
+    /// Assumptions:
+    /// 1. Azure Machine Learning Predictive Web Service is deployed
+    /// </summary>
+    public class WebServiceConsumer
+    {
+        /// <summary>
+        /// Predictive Service Url
+        /// </summary>
+        public string ServiceUrl { get; }
+        /// <summary>
+        /// Predictive service Api key
+        /// </summary>
+        public string ApiKey { get; }
+        /// <summary>
+        /// Constructor accepts Predictive WebService url and Api Key
+        /// </summary>
+        /// <param name="webServiceUrl"></param>
+        /// <param name="apiKey"></param>
+        public WebServiceConsumer(string webServiceUrl, string apiKey)
+        {
+            ServiceUrl = webServiceUrl;
+            ApiKey = apiKey;
+        }
+        /// <summary>
+        /// Consume method.
+        /// With prepared request ask WebService for Score.
+        /// Notice additional parameters in url.
+        /// </summary>
+        /// <param name="scoreRequest"></param>
+        /// <returns>tuple with two strings. first is success/failure message, second is json response</returns>
+        public async Task<Tuple<string, string>> RequestScore(Request scoreRequest)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ApiKey);
+                var response = await client.PostAsJsonAsync(ServiceUrl + "/execute?api-version=2.0&details=true", scoreRequest);
+                if (response.IsSuccessStatusCode)
+                    return new Tuple<string, string>("Request executed successfully.", await response.Content.ReadAsStringAsync());
+                return new Tuple<string, string>(await GetFailedResponse(response), null);
+            }
+        }
+        /// <summary>
+        /// In case of failure, compose user friendly error message.
+        /// </summary>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        private async Task<string> GetFailedResponse(HttpResponseMessage response)
+        {
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var sb = new StringBuilder();
+            sb.AppendLine($"The request failed with status code: {response.StatusCode}");
+            sb.AppendLine(response.Headers.ToString());
+            sb.AppendLine(responseContent);
+            return sb.ToString();
+        }
+    }
+    /// <summary>
+    /// Used for proper request preparation
+    /// </summary>
+    public class StringTable
+    {
+        public string[] ColumnNames { get; set; }
+        public string[,] Values { get; set; }
+    }
+    /// <summary>
+    /// Request to be serialized to json.
+    /// </summary>
+    public class Request
+    {
+        public Dictionary<string, StringTable> Inputs { get; set; }
+        public Dictionary<string, string> GlobalParameters { get; set; }
+    }
+}
---
+++ MachineLearningConsumerTests/MLConsumerTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using MachineLearningConsumer;
+using NUnit.Framework;
+
+namespace MachineLearningConsumerTests
+{
+    [TestFixture]
+    // ReSharper disable once InconsistentNaming
+    public class MLConsumerTests
+    {
+        /// <summary>
+        /// Url to Machine Learning Web Service
+        /// </summary>
+        private const string ServiceUrl =
+                "https://europewest.services.azureml.net/workspaces/<workspace>/services/<service>";
+
+        /// <summary>
+        /// ApiKey for MachineLearning We Service
+        /// </summary>
+        private const string ApiKey = "<api-key>";
+
+        /// <summary>
+        /// Test data based on Microsoft Sample 5:
+        /// Train, Test, Evaluate for Binary Classification: Adult Dataset
+        /// </summary>
+        private readonly Request _scoreRequest = new Request
+        {
+            Inputs = new Dictionary<string, StringTable>
+            {
+                {
+                    "input1",
+                    new StringTable
+                    {
+                        ColumnNames = new[]
+                        {
+                            "age", "workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation",
+                            "relationship", "race", "sex", "capital-gain", "capital-loss", "hours-per-week",
+                            "native-country", "income"
+                        },
+                        Values = new[,]
+                        {
+                            {
+                                "0", "value", "0", "value", "0", "value", "value", "value", "value", "value", "0", "0",
+                                "0",
+                                "value", "value"
+                            },
+                            {
+                                "0", "value", "0", "value", "0", "value", "value", "value", "value", "value", "0", "0",
+                                "0",
+                                "value", "value"
+                            }
+                        }
+                    }
+                }
+            },
+            GlobalParameters = new Dictionary<string, string>()
+        };
+        [Test]
+        public void TestConsume()
+        {
+            var consumer = new WebServiceConsumer(ServiceUrl, ApiKey);
+            var response = consumer.RequestScore(_scoreRequest);
+            response.Wait();
+            Assert.NotNull(response.Result.Item2);
+        }
+    }
+}
